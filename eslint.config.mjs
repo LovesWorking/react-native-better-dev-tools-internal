8deb527d806448c3fa85ad41d0bb6add6d753f38
lovesworking <austinlovesworking@gmail.com>
2025-08-22 17:29:47 -0600

    your message

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig([
     rules: {
       'react/react-in-jsx-scope': 'off',
       'prettier/prettier': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
   {
